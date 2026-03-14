210f02d27281160dd0b57f19c67f731ff224e922
edmex71 <edibalin@gmail.com>
2026-03-14 03:46:58 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-async function iniciarMotor(ruta){
- const casetas = await cargarCasetas();
- const detectadas = detectarCasetasRuta(ruta, casetas);
- console.log(detectadas);
- return detectadas;
-}
