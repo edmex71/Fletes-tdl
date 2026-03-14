74f566847bcce9faf9dffa533e02580d7ddcdf44
edmex71 <edibalin@gmail.com>
2026-03-14 13:27:48 -0600

    Delete geocoder.js

--- geocoder.js
+++
@@ -1,8 +0,0 @@
-
-async function geocode(city){
- const url=`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(city)}`
- const r=await fetch(url)
- const data=await r.json()
- if(!data.length) throw "Ciudad no encontrada"
- return [parseFloat(data[0].lon),parseFloat(data[0].lat)]
-}
