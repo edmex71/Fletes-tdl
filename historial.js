b892b4a799b2e0c623e30b4e72f134aade4ec510
edmex71 <edibalin@gmail.com>
2026-03-14 13:27:56 -0600

    Delete historial.js

--- historial.js
+++
@@ -1,6 +0,0 @@
-
-function guardarHistorial(data){
-let historial = JSON.parse(localStorage.getItem("historial")||"[]");
-historial.push(data);
-localStorage.setItem("historial",JSON.stringify(historial));
-}
