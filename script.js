4ffe70f656342ec22aca23ba8dc15f4a0c6ce086
edmex71 <edibalin@gmail.com>
2026-03-14 04:27:42 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+function estado(txt){
+ const el=document.getElementById('estadoMotor');
+ if(el){el.innerHTML=txt;}
+}
+
+function inicioAnalisis(){
+ estado("🔍 Analizando casetas...");
+}
+
+function finAnalisis(c){
+ estado("✅ Casetas detectadas: "+c);
+}
+
+function sinCasetas(){
+ estado("⚠️ No se detectaron casetas");
+}
+
+
 async function iniciarMotor(ruta){
  const casetas = await cargarCasetas();
  const detectadas = detectarCasetasRuta(ruta, casetas);
