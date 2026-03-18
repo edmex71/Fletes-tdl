f7c744c7b987e1931453e8f7e3f1575eefb1ef33
edmex71 <edibalin@gmail.com>
2026-03-18 17:51:52 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+
 let map;
-function initMap(){map=L.map('map').setView([19.4326,-99.1332],6);L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution:'© OpenStreetMap'}).addTo(map);}window.onload=function(){initMap();};
+
+function crearMapa(){
+  if(map){
+    map.remove();
+  }
+  map = L.map('map').setView([19.4326,-99.1332],6);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution:'© OpenStreetMap'
+  }).addTo(map);
+}
+let map;
+function initMap(){map=L.map('map').setView([19.4326,-99.1332],6);L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution:'© OpenStreetMap'}).addTo(map);}
 
 function initMapSafe(){
   try{
