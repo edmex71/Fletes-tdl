5ab5f608e9834785c0abaa81a0cc41feb969f831
edmex71 <edibalin@gmail.com>
2026-03-18 20:28:35 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-
-let map;
-
-window.onload = function() {
-  iniciarMapa();
-};
-
-function iniciarMapa(){
-  map = L.map('map').setView([19.43,-99.13],6);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution:'© OpenStreetMap'
-  }).addTo(map);
-}
-
-function calcularRuta(){
-  console.log("Ruta calculada");
-}
+console.log('MAPA IFRAME OK');
